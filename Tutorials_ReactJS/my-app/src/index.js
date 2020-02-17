a38d09e22d292ae49b31abf388a8b5ce215d80e8
Andrew Chen <43127177+achen4824@users.noreply.github.com>
2020-02-17 16:23:39 +1030

    Completed working tic tac toe react game with own code

--- Tutorials_ReactJS/my-app/src/index.js
+++ Tutorials_ReactJS/my-app/src/index.js
@@ -11,13 +11,14 @@ class Square extends React.Component {
       number : this.props.number,
     };
   }
+  //props gets values passed by called element
   render() {
     return (
       <button
         className="square"
-        onClick={() => this.setState({value : this.props.onClick(this.state.number)})}
+        onClick={() => {this.props.onClick(this.state.number)}}
       >
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
@@ -30,64 +31,84 @@ class Board extends React.Component {
     this.state = {
       move: 0,
       square: Array(9).fill(null),
+      winner: null
     };
   }
 
+  declareWinner(winner){
+    this.setState({winner: winner});
+  }
+
   handleClick(i){
-    if(this.state.move % 2 === 0){
-      this.state.square[i] = 'X';
+
+    //better create temps and set state later
+    var tempSquare = this.state.square;
+    var tempMove = this.state.move;
+
+    if(tempSquare[i] == null){
+      if(tempMove % 2 === 0){
+        tempSquare[i] = 'X';
+      }else{
+        tempSquare[i] = 'O';
+      }
     }else{
-      this.state.square[i] = 'O';
+      return tempSquare[i];
     }
+    var winner = false;
 
-
-
-    console.log(this.state.square);
-    var winner = true;
+    console.log(tempSquare);
 
     //check row
     for(var j=0;j<3;j++){
-      if(this.state.square[(Math.floor(i / 3) * 3) + j] !== this.state.square[i]){
+      winner = true;
+      if(tempSquare[(Math.floor(i / 3) * 3) + j] !== tempSquare[i]){
         winner = false;
         break;
       }
     }
-
+    if(winner){this.declareWinner(tempSquare[i]);}
     //check column
     for(j=0;j<3;j++){
-      if(this.state.square[(i % 3) + (j * 3)] !== this.state.square[i] || winner === false){
+      winner = true;
+      if(tempSquare[(i % 3) + (j * 3)] !== tempSquare[i] || winner === false){
         winner = false;
         break;
       }
     }
-
+    if(winner){this.declareWinner(tempSquare[i]);}
     //check if in postion where we have to check diagonals
-    if(!((i - 1 % 3 === 0 || i === 3 || i === 5) && i !== 4) && winner === true){
+    if(!((i - 1 % 3 === 0 || i === 3 || i === 5) && i !== 4) && winner === false){
       if(i === 0 || i === 8 || i === 4){
+        winner = true;
         for(var f = 0;f<=8;f += 4){
-          if(this.state.square[f] !== this.state.square[i]){
+          if(tempSquare[f] !== tempSquare[i]){
             winner = false;
             break;
           }
         }
+        if(winner){this.declareWinner(tempSquare[i]);}
       }
       if(i == 2 || i == 6 || i == 4){
+        winner = true;
         for(f = 2;f<=6;f += 2){
-          if(this.state.square[f] !== this.state.square[i]){
+          if(tempSquare[f] !== tempSquare[i]){
             winner = false;
             break;
           }
         }
+        if(winner){this.declareWinner(tempSquare[i]);}
       }
     }
+    tempMove++;
 
-    if(winner){
-      console.log(this.state.square[i] + " Wins");
-    }
-    this.state.move++;
-    return this.state.square[i];
+    //updates html by calling render meaning all funcs will be recalled
+    this.setState({
+      move : tempMove,
+      square : tempSquare
+    });
   }
 
+  //is updated in the render call
   renderSquare(i) {
     return <Square
               number = {i}
@@ -97,7 +118,11 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    let status;
+    status = 'Next player: ' + (this.state.move % 2 == 0 ? 'X' : 'O');
+    if(this.state.winner != null){
+      status = 'Winner is ' + this.state.winner;
+    }
 
     return (
       <div>
