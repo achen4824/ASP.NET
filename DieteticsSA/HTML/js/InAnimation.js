63dfb214aaee1ac7f648c12c8ce7d771068641da
Andrew Chen <43127177+achen4824@users.noreply.github.com>
2020-02-18 15:45:51 +1030

    created animations for resizing

--- DieteticsSA/HTML/js/InAnimation.js
+++ DieteticsSA/HTML/js/InAnimation.js
@@ -1,15 +1,37 @@
 $(document).ready(function() {
-    // all custom jQuery will go here
-    
-    $("#intro").css("width", "575px");
-    $("#intro").css("height", "150px");
-    $("#intro").css("margin-top", "75px");
-    $("#logo").css("transform", "rotate(0deg)");
-
-    //second stage opening
-    setTimeout(()=>{
-        $("#seperator").css("height","75%");
-        $("#title").css("opacity","1");
-        $("#details").css("opacity","1");
-    },300);
+    initanimation();
 });
+
+//detect resize and remove large elements
+$(window).resize(function() {
+    initanimation();
+});
+
+function initanimation(){
+    //resize just happened, pixels changed
+    const vw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+
+    //large screens
+    if(vw > 700){
+        $("#intro").css("width", "550px");
+        $("#intro").css("height", "150px");
+        $("#intro").css("margin-top", "75px");
+        $("#logo").css("transform", "rotate(0deg)");
+
+        //second stage opening
+        setTimeout(()=>{
+            $("#seperator").css("height","75%");
+            $("#title").css("opacity","1");
+            $("#details").css("opacity","1");
+        },300);
+
+    //small screens
+    }else{
+        $("#logo").css("transform", "rotate(0deg)");
+        $("#intro").css("width", "150px");
+        $("#intro").css("height", "150px");
+        $("#intro").css("margin-top", "75px");
+        $("#seperator").css("height","0%");
+        $("#details").css("opacity", "0");
+    }
+}
